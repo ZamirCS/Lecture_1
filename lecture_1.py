0e6e209a9a1312aea113597e69befbb6119e0420
ZamirCS <zamir357@icloud.com>
2024-09-03 16:59:46 -0400

    Lecture of 09/03/2024

---
+++ lecture_1.py
@@ -0,0 +1,13 @@
+#Lecture of 09/03/2024
+money = 14
+movie_cost = 20
+
+if(money>movie_cost):
+    print('We can see the movie')
+
+else:
+    print('We cannot see the movie')
+
+print('I am out of the if statement')
+
+
